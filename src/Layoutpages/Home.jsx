87b6b4a5adccc3255d5396ca581ba1801776601c
talhabadar184 <l1f22bscs0406@ucp.edu.pk>
2025-08-17 12:29:28 +0500

    third commit

--- src/Layoutpages/Home.jsx
+++ src/Layoutpages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Home1 from './Home1'
 import Products from './Products';
 import About from './About'
-import Contact from '../components/contact';
+import Contact from '../components/Contact';
 import Navbar from '../components/Navbar';
 
 function Home() {
